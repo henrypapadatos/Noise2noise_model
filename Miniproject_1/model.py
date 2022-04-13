b6f7f1c09d6c9c433e5213379f5f5065e333db7a
Henry papadatos <papadatoshenry@gmail.com>
2022-04-13 12:47:49 +0200

    Implementation of psnr and new normalization The model is now trainable

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -9,7 +9,7 @@ class Model(nn.Module):
         super().__init__()
         self.lr = 0.001
         self.nb_epoch = 100
-        self.batch_size = 500
+        self.batch_size = 100
 
         self.model = nn.Sequential(
                           nn.Conv2d(3, 32, kernel_size=(5,5), stride=(1,1)),
@@ -42,7 +42,7 @@ class Model(nn.Module):
         ## This loads the parameters saved in bestmodel .pth into the model
         pass
 
-    def train(self, train_input, train_target):
+    def train(self, train_input, train_target, test_input, test_target):
         #: train˙input : tensor of size (N, C, H, W) containing a noisy version of the images
         #: train˙target : tensor of size (N, C, H, W) containing another noisy version of the
         # same images , which only differs from the input by their noise .
@@ -50,54 +50,53 @@ class Model(nn.Module):
             i = 0
             for input, targets in zip(train_input.split(self.batch_size),  
                                       train_target.split(self.batch_size)):
-                output = self.model(input)
+                output = self.predict(input)
                 loss = self.criterion(output, targets)
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
                 i+=1
                 #print("Nb of batch is: "+str(i))
-            print("Nb of epoch is: "+str(e))
-        
+            
+            denoised = self.predict(test_input)
+            psnr = self.psnr(denoised, test_target)
+            
+            print('Nb of epoch: {:d}    psnr: {:.02f}'.format(e, psnr))
 
-        pass
-
-    def predict(self, test_input):
-
+    def predict(self, input_imgs):
         #: test˙input : tensor of size (N1 , C, H, W) that has to be denoised by the trained
         # or the loaded network .
-        pass
+        output = self.model(input_imgs)
+        return output
+    
+    def psnr(self, denoised, ground_truth):
+        #Peak Signal to Noise Ratio : denoised and ground˙truth have range [0 , 1]
+        mse = torch.mean((denoised - ground_truth )** 2)
+        return -10 * torch . log10 ( mse + 10** -8)
+
     
 def display_img(img):
     
     image = img.permute(1,2,0)
-
-    #if the image is float type, it has been normalized. We need to denormalize it before showing
-    if image.dtype == torch.float32:
-        image = (image*std+mean).int()
     
     plt.figure()
     plt.imshow(image)
     
 #######################################################################################
 
-subset = 1000
+subset_train = 1000
+subset_test = 100
 val_subste = 100
 noisy_imgs_1 , noisy_imgs_2 = torch.load('train_data.pkl')
+
+max_pxl = noisy_imgs_1.max().item()
 #We neet float type to 
-noisy_imgs_1 = noisy_imgs_1[0:subset,:,:,:].float()
-noisy_imgs_2 = noisy_imgs_2[0:subset,:,:,:].float()
+noisy_imgs_1 = noisy_imgs_1[0:subset_train,:,:,:]/max_pxl
+noisy_imgs_2 = noisy_imgs_2[0:subset_train,:,:,:]/max_pxl
 
-mean, std = noisy_imgs_1.mean(), noisy_imgs_1.std()
-noisy_imgs_1.sub_(mean).div_(std)
-noisy_imgs_2.sub_(mean).div_(std)
-
-noisy_imgs , clean_imgs = torch.load ('val_data.pkl')
-noisy_imgs = noisy_imgs_1[0:subset,:,:,:].float()
-clean_imgs = noisy_imgs_2[0:subset,:,:,:].float()
-
-noisy_imgs.sub_(mean).div_(std)
-clean_imgs.sub_(mean).div_(std)
+test_imgs , clean_imgs = torch.load ('val_data.pkl')
+test_imgs = test_imgs[0:subset_test,:,:,:]/max_pxl
+clean_imgs = clean_imgs[0:subset_test,:,:,:]/max_pxl
 
 model = Model()
 
@@ -107,7 +106,7 @@ if torch.cuda.is_available():
     noisy_imgs_1 =noisy_imgs_1.cuda()
     noisy_imgs_2 = noisy_imgs_2.cuda()
 
-model.train(noisy_imgs_1, noisy_imgs_2)
+model.train(noisy_imgs_1, noisy_imgs_2, test_imgs, clean_imgs)
 
 image_number = 0
 display_img(noisy_imgs_1[image_number,:,:,:])
