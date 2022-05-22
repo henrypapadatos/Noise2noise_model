cce44a7dcb63f359525058b9fb2eb495593a624b
Aitana <aitana.waelbroeck@hotmail.com>
2022-05-22 14:36:58 +0200

    No error model.py

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -119,18 +119,18 @@ class Conv2d(Module):
 
 
 class Optimizer(Module):
-    def __init__(self, params, lr, momentum=0, dampening=0, weight_decay=0, nesterov=False, *, maximize=False):
+    def __init__(self, lr, momentum=0, dampening=0, weight_decay=0, nesterov=False, maximize=False):
         #self.momentum = momentum
-        self.params = params
         #self.dampening = dampening
         #self.weight_decay = weight_decay
         #self.nesterov = nesterov
         self.maximize = maximize
         self.lr = lr
         #self.prev_b = 0
-    def forward (self , x) :
-        for params in self.params:
-            for param in params:
+    def step (self, params) :
+
+        for param_layer in params:
+            for param in param_layer:
                 g_t = param[1]
                 #if self.weight_decay:
                     #g_t = g_t + self.weight_decay*param[0]
@@ -148,18 +148,11 @@ class Optimizer(Module):
                     param[0] = param[0] + self.lr*g_t
                 else:
                     param[0] = param[0] - self.lr*g_t
-        return params
-
-    
-    def backward (self,y):
-        return []
 
     def param ( self ) :
         return [self.params]
 
 
-
-
 def training_visualisation(imgs):
     #Plot the 4 first images of imgs in a subplot way
     fig=plt.figure()
@@ -182,103 +175,12 @@ class Model():
         self.lr = 0.002
         self.nb_epoch = 100
         self.batch_size = 1000
-        self.optimizer = None
+        self.optimizer = Optimizer(lr= self.lr)
         self.criterion = MSE()
-        self.Conv = Conv2d()
-        self.ReLU = Relu()
-        self.Sigmoid = Sigmoid()
-        #self.Upsampling = Upsampling()
-        #self.model = Sequential(self.Conv(3,3,3) , self.ReLU , self.Conv(3,3,3) , self.ReLU , self.Upsampling , self.ReLU , self.Upsampling , self.Sigmoid)
-        self.model = Sequential()
-
-    
-    def load_pretrained_model(self):
-        ## This loads the parameters saved in bestmodel .pth into the model$
-        #full_path = os.path.join('Miniproject_2', 'bestmodel.pth')
-        #self.model.load_state_dict(torch.load(full_path,map_location=torch.device('cpu')))
-        pass 
-    def train(self, train_input, train_target, num_epochs=100 ,test_input=None, test_target=None, vizualisation_flag = False):
-        num_epochs = 10
-
-        if vizualisation_flag and test_input!=None:
-            plt.ion()
-            plt.show()
-            training_visualisation(test_target)
-            training_visualisation(test_input)
-        
-        if test_input!=None:
-            initial_psnr = self.psnr(test_input/255, test_target/255)
-            print('Psnr value between clean and noisy images is: {:.02f}'.format(initial_psnr))
-
-        for e in range(num_epochs):
-            i = 0
-            for input, targets in zip(train_input.split(self.batch_size),  
-                                      train_target.split(self.batch_size)):
-
-                
-                output = self.model.forward(input)
-                loss = self.criterion.forward(output/255, targets/255)
-                grad_loss = self.criterion.backward()
-                self.model.backward(grad_loss)
-                #self.optimizer.step()
-                i+=1
-            
-            denoised = self.model.forward(test_input)
-
-            if test_input!=None:    
-                psnr = self.psnr(denoised/255, test_target/255)                
-                print('Nb of epoch: {:d}    psnr: {:.02f}'.format(e, psnr))
-                
-            #display denoised images 25 times during training
-            if vizualisation_flag and (e%(num_epochs//10)==0) and test_input!=None:
-                training_visualisation(denoised)
-            
-        
-    def predict(self, input_imgs):
-        #: test˙input : tensor of size (N1 , C, H, W) that has to be denoised by the trained
-        # or the loaded network .
-        #normalize image between 0 and 1
-        input_imgs_ = input_imgs/255
-        output = self.model.forward(input_imgs_)
-
-        # output should be an int between [0,255]
-        output = torch.clip(output*255, 0, 255)
-        return output
-    
-    def psnr(self, denoised, ground_truth):
-        #Peak Signal to Noise Ratio : denoised and ground˙truth have range [0 , 1]
-        mse = torch.mean((denoised - ground_truth )** 2)
-        psnr = -10 * torch . log10 ( mse + 10** -8)
-        return psnr.item()
-
-def training_visualisation(imgs):
-    #Plot the 4 first images of imgs in a subplot way
-    fig=plt.figure()
-    nb_image = 4
-    for a in range(nb_image):
-        with torch.no_grad():     
-            b = imgs[a,:,:,:].int()
-            if torch.cuda.is_available():
-                b = b.to('cpu')
-            b = b.permute(1,2,0)    
-            ax = fig.add_subplot(1, nb_image, a+1)
-            subplot_title=("Image"+str(a))
-            ax.set_title(subplot_title)  
-            plt.imshow(b)
-    fig.tight_layout() 
-    plt.pause(0.001)
-    
-class Model():
-    def __init__(self):
-        self.lr = 0.002
-        self.nb_epoch = 100
-        self.batch_size = 1000
-        self.optimizer = None
-        self.criterion = MSE()
-        self.Conv = Conv2d()
-        self.ReLU = Relu()
-        self.Sigmoid = Sigmoid()
-        #self.Upsampling = Upsampling()
+        self.Conv = Conv2d
+        self.ReLU = Relu
+        self.Sigmoid = Sigmoid
+        #self.Upsampling = Upsampling
         #self.model = Sequential(self.Conv(3,3,3) , self.ReLU , self.Conv(3,3,3) , self.ReLU , self.Upsampling , self.ReLU , self.Upsampling , self.Sigmoid)
         self.model = Sequential()
 
@@ -312,7 +214,7 @@ class Model():
                 grad_loss = self.criterion.backward()
                 self.model.backward(grad_loss)
                 # add SGD
-                #self.optimizer.step()
+                self.optimizer.step(self.model.param())
                 i+=1
             
             denoised = self.model.forward(test_input)
